6bd55e2acb7116e5bf440579a675cb0d139274f0
Cristian <ema9948@gmail.com>
2022-09-05 21:18:39 -0300

    0.0.1

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -23,10 +23,10 @@ export const Portfolio = () => {
           {dataportfolio.map((data, i) => {
             return (
               <div key={i} className="po_item">
-                <img src={pro1} alt="" />
+                <img src={pro1} alt="" className="img" />
                 <div className="content">
                   <p>{data.desctiption}</p>
-                  <a href={data.link}>view project</a>
+                  <a href={data.link} target="_blanck" >view project</a>
                 </div>
               </div>
             );
